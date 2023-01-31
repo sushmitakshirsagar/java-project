299b5ab96f03e5fe779c1026fe21010bafc011c3
Amol Shinde <sushmitakshirsagar>
2023-01-31 15:12:44 +0530

    java project added

---
+++ src/abc.java
@@ -0,0 +1,14 @@
+
+public class abc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hi");
+		System.out.println("how are you");
+		System.out.println("I am fine");
+		System.out.println(12+12);
+		System.out.println(12*12);
+
+	}
+
+}
